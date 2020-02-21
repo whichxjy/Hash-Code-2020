772a6f4a4b514615f4be20c87a63fed6fe18e9a0
whichxjy <whichxjy@gmail.com>
2020-02-21 09:47:17 +0800

    Update

--- practice-round/run.sh
+++ practice-round/run.sh
@@ -1,6 +1,6 @@
 mkdir -p output
-python dp.py -f input/a.in -o output/a.txt
-python dp.py -f input/b.in -o output/b.txt
-python dp.py -f input/c.in -o output/c.txt
-python navie.py -f input/d.in -o output/d.txt
-python navie.py -f input/e.in -o output/e.txt
+python src/dp.py -f input/a.in -o output/a.txt
+python src/dp.py -f input/b.in -o output/b.txt
+python src/dp.py -f input/c.in -o output/c.txt
+python src/navie.py -f input/d.in -o output/d.txt
+python src/navie.py -f input/e.in -o output/e.txt
